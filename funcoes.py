dc0bd50ce7d897a9236f55f73868a710c95d2bc4
Guilherme Hosoda Souza Reis <guilhermehosoda@gmail.com>
2022-02-23 12:44:06 -0300

    FInalizacao IEEE112MetodoA

--- funcoes.py
+++ funcoes.py
@@ -16,26 +16,30 @@ from math import pi
 
 
 ############################################################
-# %%          CLASSE PAI
+# %%         CLASSE PAI
 ############################################################
 class Ensaio:
     def __init__(self):
-        self.__colunas = []
-        self.__tabelas = {}
-        self.__dfs = {}
+        self._colunas = []
+        self._tabelas = {}
+        self._dfs = {}
+        self._incertezas = {}
     
     @property
     def tabelas(self):
-        return self.__tabelas
+        return self._tabelas
     
     @property
     def colunas(self):
-        return self.__colunas
+        return self._colunas
     
     @property
     def dfs(self):
-        return self.__dfs
+        return self._dfs
  
+    @property
+    def incertezas(self):
+        return self._incertezas
     
 ############################################################
 # %%          CLASSE IEEE112
@@ -43,15 +47,15 @@ class Ensaio:
 class IEEE112(Ensaio):
     def __init__(self):
         super().__init__()
-        self.__tabelas = ['Resistencias', 'Ensaio de Carga', 'Ensaio a Vazio']
-        self.__colunas = {'Resistencias': ['RS', 'RT', 'ST', 'T_amb', 'T_res'],
+        self._tabelas = ['Resistencias', 'Ensaio de Carga', 'Ensaio a Vazio']
+        self._colunas = {'Resistencias': ['RS', 'RT', 'ST', 'T_amb', 'T_res'],
                           'Ensaio de Carga': ['Tensao', 'Corrente', 'Potencia', 'Frequencia', 'Temperatura', 'Torque', 'RPM'],
-                          'Ensaio a Vazio': ['Tensao', 'Corrente', 'Potencia', 'Frequencia']}
-    
+                          'Ensaio a Vazio': ['Tensao', 'Corrente', 'Potencia', 'Frequencia']}  
+     
     def drop_ensaios(self, ensaios_manter):
-        self.__tabelas = ensaios_manter
-        self.__colunas = {chave:valor for chave, valor in self.__colunas.items() if chave in ensaios_manter}
-            
+        self._tabelas = ensaios_manter
+        self._colunas = {chave:valor for chave, valor in self._colunas.items() if chave in ensaios_manter}
+
     def ensaio_resistencia(self, df, **kwargs):
         #Configuração KWARGS
         dici = {'Material Estator': 'Cobre'}
@@ -71,12 +75,12 @@ class IEEE112(Ensaio):
         #Inserir cálculo do T Final??
         df['Resistencias'] = tab_resist
         return df
-    
-    def ensaio_carga(self, df, *kwargs):
+
+    def ensaio_carga(self, df, **kwargs):
         #Configuração KWARGS
         dici = {'Material Estator': 'Cobre',
                 'Material Rotor': 'Alumínio',
-                'polos': 4}
+                'Polos': 4}
         dici.update(kwargs)
         k = {'Cobre': 234.5,
              'Alumínio': 225.0}
@@ -86,13 +90,40 @@ class IEEE112(Ensaio):
         #Inicializando os dfs
         tab_resist = deepcopy(df['Resistencias'])
         tab_carga = deepcopy(df['Ensaio com Carga'])
+        
+        #Configurando as temperaturas
+        tt = tab_carga['Temperatura']
+        ts = tab_resist.loc[2, 'Ts']
+        
+        #Velocidade Sincrona
+        tab_carga['Velocidade_Sinc'] = 120*tab_carga['Frequencia']/dici['Polos']
+        
+        #Escorregamento
+        tab_carga['Escorregamento'] = (tab_carga['Velocidade_Sinc'] - tab_carga['Velocidade'])/tab_carga['Velocidade_Sinc']
+        fator = (ts+k_rotor)/(tt+k_rotor)
+        tab_carga['C-Escorregamento'] = tab_carga['Escorregamento']*fator
+        
+        #Velocidade Corrigida
+        tab_carga['C-Velocidade'] = tab_carga['Velocidade_Sinc']*(1 - tab_carga['C-Escorregamento'])
+        
+        #Correcao Dinamometro
+        tab_carga['C-Dinamometro'] = 0
+        
+        #Torque Corrigido
+        tab_carga['C-Torque'] = tab_carga['Torque'] + tab_carga['C-Dinamometro']
+        
+        #Potencia saida
+        tab_carga['P_out'] = 2*np.pi*tab_carga['C-Torque']*tab_carga['C-Velocidade']/60
 
+        df['Ensaio com Carga'] = tab_carga
+        return df
+    
     def ensaio_vazio(self, df, **kwargs):
         #Configuração KWARGS
         dici = {'Material Estator': 'Cobre',
                 'Material Rotor': 'Alumínio',
                 'polos': 4,
-                'Tensão (V)': 380}
+                'Tensão Nominal': 380}
         dici.update(kwargs)
         k = {'Cobre': 234.5,
              'Alumínio': 225.0}
@@ -101,210 +132,220 @@ class IEEE112(Ensaio):
         
         #Inicializando os dfs
         tab_resist = deepcopy(df['Resistencias'])
-        tab_carga = deepcopy(df['Ensaio a Vazio'])
-        pass
+        tab_vazio = deepcopy(df['Ensaio a Vazio'])
+        
+        #Cálculo Iniciais
+        tab_vazio['V^2'] = tab_vazio['Tensao']**2
+        tab_vazio['FP'] = tab_vazio['Potencia']/(np.sqrt(3)*tab_vazio['Corrente']*tab_vazio['Tensao'])        
+        r1_medio = float(tab_resist.loc[4, 'R1'] + tab_resist.loc[3,'R1'])/2
+        
+        #Cálculo Pin - Pj1
+        tab_vazio['Pin-Pj1'] = tab_vazio['Potencia'] -3*r1_medio*tab_vazio['Corrente']**2
+        
+        #Cálculo P_fw - Corrigir escolha das listas
+        pin_pj1 = list(tab_vazio['Pin-Pj1'])
+        v2 = list(tab_vazio['V^2'])
+        v2 = sm.add_constant(v2)
+        resultado_regressao = sm.OLS(pin_pj1, v2).fit()
+        tab_vazio['P_fw'] = float(resultado_regressao.params[0])
+        
+        #Cálculo P_ferro
+        tab_vazio['P_fe'] = tab_vazio['Pin-Pj1'] - tab_vazio['P_fw']
+        
+        #Voltando pra variável inicial
+        df['Ensaio a Vazio'] = tab_vazio
+        return df
+  
+    def perdas_suplementares(self, p_sup, t2):
+        while True:
+            corr_fac = .8
+            coeff_ang = 0
+            if coeff_ang < 0 or corr_fac < .9:
+                aux = {}
+                for i in range(len(p_sup)):
+                    p_sup_aux = deepcopy(p_sup)
+                    p_sup_aux.pop(i)
+                    t2_aux = deepcopy(t2)
+                    t2_aux.pop(i)
+                    t2_aux = sm.add_constant(t2_aux)
+                    resultado_regressao = sm.OLS(p_sup_aux, t2_aux).fit()
+                    aux[i] = {'lin': resultado_regressao.params[1],
+                              'corr_fac': resultado_regressao.cov_type}
+                    print(aux[i])
+            else:
+                break
+            break
+        return coeff_ang
+######################################
+# %%    CLASSE IEEE112 - PROPERTIES
+###################################### 
+    @property
+    def tabelas(self):
+        return self._tabelas
     
+    @property
+    def colunas(self):
+        return self._colunas
+    
+
 ############################################################
 # %%          CLASSE IEEE 112 MÉTODO A
 ############################################################
 class IEEE112MetodoA(IEEE112):
     def __init__(self):
         super().__init__()
-        ensaios_manter = ['Resistências', 'Ensaio de Carga']
+        ensaios_manter = ['Resistencias', 'Ensaio de Carga']
         self.drop_ensaios(ensaios_manter)
-        print(self.__tabelas)
-        print(self.__colunas)
+        self._incertezas = {'Corrente': ['Corrente'],
+                 'Tensao': ['Tensao'],
+                 'Potencia': ['Potencia'],
+                 'Frequencia': ['Frequencia'],
+                 'Resistencia': ['RS', 'RT', 'ST'],
+                 'Torque': ['Torque'],
+                 'RPM': ['Velocidade'],
+                 'Temperatura': ['Temperatura', 'T_amb', 'T_res']}
           
-    def incertezas(self, dfs, dicionario_incertezas):
-        pass
+    def calculo(self, dfs_mc, **kwargs):
+        for dfs in dfs_mc:
+            dfs = self.ensaio_resistencia(dfs, **kwargs)
+            dfs = self.ensaio_carga(dfs, **kwargs)
+            dfs = self.rendimento(dfs, **kwargs)
+        return dfs
     
-    def calculo(self, dfs):
-        pass
-
+    def rendimento(self, dfs, **kwargs):
+        #Inicializando os KWARGS
+        dici = {'Material Estator': 'Cobre',
+                'Potencia Nominal': 1}
+        dici.update(kwargs)
+        k = {'Cobre': 234.5,
+             'Alumínio': 225.0}
+        k_estator = k[dici['Material Estator']]
+        p_nominal = dici['Potencia Nominal']
+        
+        #Inicializando os dfs
+        tab_resist = deepcopy(dfs['Resistencias'])
+        tab_carga = deepcopy(dfs['Ensaio com Carga'])
+        
+        #Variáveis
+        tf = tab_resist.loc[0,'T_amb']
+        ts = tab_resist.loc[1, 'Ts']
+        R1 = tab_resist.loc[0, 'R1']
+        
+        #Cálculo Pj1 -> Tt
+        tab_carga['Pj1 - Tt'] = 1.5*tab_carga['Corrente']**2*R1*(k_estator+tab_carga['Temperatura'])/(k_estator+tf)
+        
+        #Calculo Pj1 -> Ts
+        tab_carga['Pj1 - Ts'] = 1.5*tab_carga['Corrente']**2*R1*(k_estator+ts)/(k_estator+tf)
+        dfs['Ensaio com Carga'] = tab_carga
+        #Correção Pj1
+        tab_carga['C-Pj1'] = tab_carga['Pj1 - Ts'] - tab_carga['Pj1 - Tt']
+        
+        #Correção Pin
+        tab_carga['C-Pin'] = tab_carga['Potencia'] + tab_carga['C-Pj1']
+        
+        #Cálculo rendimento
+        tab_carga['Rendimento'] = tab_carga['P_out']/tab_carga['C-Pin']
+        
+        #Criação novo Df com o resultado
+        resultado = tab_carga[['Potencia', 'C-Escorregamento', 'C-Pj1', 'C-Pin', 'P_out', 'Rendimento']]
+        resultado['Potencia'] = resultado['Potencia']/p_nominal
+        dfs['Resultado'] = resultado
+        return dfs
+    
 
 ############################################################
 # %%          CLASSE IEEE 112 MÉTODO B
 ############################################################
 class IEEE112MetodoB(IEEE112):
     def __init__(self):
-        self.__tabelas = ['Resistencias', 'Ensaio de Carga', 'Ensaio a Vazio']
-        self.__colunas = {'Resistencias': ['RS', 'RT', 'ST', 'T_amb', 'T_res'],
-                          'Ensaio de Carga': ['Tensao', 'Corrente', 'Potencia', 'Frequencia', 'Temperatura', 'Torque', 'RPM'],
-                          'Ensaio a Vazio': ['Tensao', 'Corrente', 'Potencia', 'Frequencia']}
-    
-    def incertezas(self, dfs, dicionario_incertezas):
-        pass
+        super().__init__()
+        ensaios_manter = ['Resistencias', 'Ensaio de Carga', 'Ensaio a Vazio']
+        self.drop_ensaios(ensaios_manter)
+        
+    def incertezas(self):
+        dici  = {'Corrente': ['Corrente'],
+                 'Tensao': ['Tensao'],
+                 'Potencia': ['Potencia'],
+                 'Frequencia': ['Frequencia'],
+                 'Resistencia': ['RS', 'RT', 'ST'],
+                 'Torque': ['Torque'],
+                 'RPM': ['Velocidade'],
+                 'Temperatura': ['Temperatura', 'T_amb', 'T_res']}
+        return dici
 
-    def calculo(self, dfs):
-        pass
+    def calculo(self, dfs_mc, **kwargs):
+        for dfs in dfs_mc:
+            dfs = self.ensaio_resistencia(dfs, **kwargs)
+            dfs = self.ensaio_carga(dfs, **kwargs)
+            dfs = self.ensaio_vazio(dfs, **kwargs)
+            dfs = self.rendimento(dfs, **kwargs)
+        return dfs_mc
 
-############################################################
-# %%          CÓDIGO ANTIGO
-############################################################
+    def rendimento(self, dfs, **kwargs):
+        #Inicializando os KWARGS
+        dici = {'Material Estator': 'Cobre',
+                'Potencia Nominal': 1,
+                'Tensao Nominal': 380}
+        dici.update(kwargs)
+        k = {'Cobre': 234.5,
+             'Alumínio': 225.0}
+        k_estator = k[dici['Material Estator']]
+        p_nominal = dici['Potencia Nominal']
+        t_nominal = dici['Tensao Nominal']
+        
+        #Inicializando os dfs
+        tab_resist = deepcopy(dfs['Resistencias'])
+        tab_carga = deepcopy(dfs['Ensaio com Carga'])
+        tab_vazio = deepcopy(dfs['Ensaio a Vazio'])
+        
+        #Variáveis
+        t_nom_index = 1#Método para quebrar um galho
+        
+        #Atribuição das perdas de atrito e ventilação para o dataframe do ensaio de carga
+        tab_carga['P_fw'] = float(tab_vazio.loc[0, 'P_fw'])
+        
+        #Perda no ferro Condicoes de ensaio
+        p_fe_ensaio = tab_vazio.loc[t_nom_index, 'P_fe']*(t_nominal/tab_vazio.loc[t_nom_index, 'Tensao'])**2
+        tab_carga['P_fe'] = p_fe_ensaio*(tab_carga['Tensao']/t_nominal)**2
+        
+        #Pj1
+        tt = tab_carga['Temperatura']
+        tTR = tab_resist.loc[2, 'Ts']
+        tTTD = tab_resist.loc[2, 'T_res']
+        tA = tTR*tt/tTTD
+        R1 = tab_resist.loc[2, 'R1']*(k_estator+tA)/(k_estator+tTR)
+        tab_carga['Pj1'] = 1.5*tab_carga['Corrente']**2*R1
+        
+        #Pj2
+        tab_carga['Pj2'] = (tab_carga['Potencia'] - tab_carga['P_fe'] - tab_carga['Pj1'])*tab_carga['Escorregamento']
+        
+        #Perdas totais converncionais
+        tab_carga['P_total_conv'] = tab_carga['Pj1']+tab_carga['Pj2']+tab_carga['P_fe']+tab_carga['P_fw']
+        
+        #Correção Pout
+        tab_carga['C-P_out'] = 2*np.pi*tab_carga['C-Torque']*tab_carga['Velocidade']/60
+        
+        #Perdas totais
+        tab_carga['P_total'] = tab_carga['Potencia'] - tab_carga['C-P_out']
+        
+        #Perdas suplementares
+        tab_carga['P_sup'] = tab_carga['P_total'] - tab_carga['P_total_conv']
+        
+        #Correção perdas suplementares
+        p_sup = list(tab_carga['P_sup'])
+        t2 = list(tab_carga['C-Torque']**2)
+        coeff = self.perdas_suplementares(p_sup, t2)
+        print(coeff)
+        
+        #Criando novo df com os resultados
+        resultado = tab_carga
+        #resultado['Potencia'] = resultado['Potencia']/p_nominal
+        dfs['Resultado'] = resultado
+        
+        return dfs
 
-def ensaio_termico(df, **kwargs):
-    # Atualização dos kwargs
-    dici = {'material_estator': 'Cobre', 'aba_termico': 'Ensaio_Termico_Vazio'}
-    k_estator = {'Cobre': 234.5, 'Alumínio': 225.0}
-    dici.update(kwargs)
-    k_estator = k_estator[dici['material_estator']]
-    dft = deepcopy(df[dici['aba_termico']])
-    # cálculo de R1 Inicial e final
-
-    dft['D - R1'] = (dft['RS']+dft['RT']+dft['ST'])/(6)
-    dft.set_index(['Tipo'], inplace=True)
-
-    # Cálculo R1@25ºC
-    dft['D - R1@25C'] = dft['D - R1'] * \
-        (25+k_estator)/(dft['Temperatura'] + k_estator)
-
-    # Cálculo T Final
-    aux = dft.loc['Final', 'D - R1'] * \
-        (25+k_estator)/dft.loc['Inicial', 'D - R1@25C'] - k_estator
-    dft['E - T Final'] = deepcopy(aux)
-
-    # Cálculo dT
-    aux = dft.loc['Final', 'E - T Final'] - dft.loc['Final', 'Temperatura']
-    dft['E - dT'] = deepcopy(aux)
-
-    # Cálculo Ts
-    aux = dft.loc['Final', 'E - dT'] + 25
-    dft['E - TS'] = deepcopy(aux)
-    df[dici['aba_termico']] = deepcopy(dft)
-    return df
-
-##############################
-
-
-def ensaio_vazio(df, **kwargs):
-    # Inicializando os kwargs
-    dici = {'aba_vazio': 'Ensaio_Vazio',
-            'fora': [420],
-            'pu': 380}
-
-    # Variáveis auxiliares
-    v2 = []
-    pin_pj1 = []
-    vcore = []
-
-    dfv = deepcopy(df[dici['aba_vazio']])
-
-    # Cálculo das resistências
-    termico = ensaio_termico(df, aba_termico='Ensaio_Termico_Vazio')
-    r1_inicial = termico['Ensaio_Termico_Vazio'].loc['Inicial', 'D - R1']
-    r1_final = termico['Ensaio_Termico_Vazio'].loc['Final', 'D - R1']
-    r1 = (r1_inicial+r1_final)/2
-
-    # Cálculo V^2
-    dfv['D - V^2'] = dfv['Tensao']*dfv['Tensao']
-
-    # Cálculo FP
-    dfv['D - FP'] = dfv['Potencia']/(dfv['Corrente']*dfv['Tensao']*np.sqrt(3))
-
-    # Cálculo Pin - Pj1
-    dfv['D - Pin - Pj1'] = dfv['Potencia'] - 3*r1*(dfv['Corrente']**2)
-
-    # Cálculo VCore
-    dfv['D - VCore'] = (dfv['Tensao'] - np.sqrt(3) *
-                        r1_inicial*dfv['Corrente']*dfv['D - FP'])**2
-    dfv['D - VCore'] += (np.sqrt(3)*dfv['Corrente'] *
-                         r1_inicial*np.sqrt(1-dfv['D - FP']**2))**2
-    dfv['D - VCore'] = np.sqrt(dfv['D - VCore'])
-
-    # Definindo Vref como indice
-    dfv.set_index('Vref', inplace=True)
-
-    v2 = [dfv.loc[vref, 'D - V^2']
-          for vref in dfv.index if not(vref in dici['fora'])]
-    pin_pj1 = [dfv.loc[vref, 'D - Pin - Pj1']
-               for vref in dfv.index if not(vref in dici['fora'])]
-    # vcore = [dfv.loc[vref, 'D - VCore'] for vref in dfv.index if not(vref in dici['fora'])]
-
-    # Cálculo das regressões lineares de V^2 e Pin - Pj1
-    v2 = sm.add_constant(v2)
-
-    resultado_regressao = sm.OLS(pin_pj1, v2).fit()
-
-    # Perdas Atrito e ventilação
-    dfv['E - Perdas Atrito Vent.'] = deepcopy(resultado_regressao.params[0])
-
-    # Cálculo Perdas Ferro
-    dfv['D - Perdas Ferro'] = dfv['D - Pin - Pj1'] - \
-        dfv['E - Perdas Atrito Vent.']
-    dfv['E - Perdas Ferro Cond Ensaio'] = float(
-        dfv.loc[dici['pu'], 'D - Pin - Pj1'] - dfv.loc[dici['pu'], 'E - Perdas Atrito Vent.'])
-    dfv['E - Perdas Ferro Tensao Nominal'] = float(
-        dfv.loc[dici['pu'], 'E - Perdas Ferro Cond Ensaio']*(dici['pu']/dfv.loc[dici['pu'], 'Tensao'])**2)
-
-    df[dici['aba_vazio']] = dfv
-    return df
-
-##############################
-
-
-def ensaio_carga(df, **kwargs):
-    # Inicializando os kwargs
-    dici = {'material_estator': 'Cobre',
-            'aba_carga': 'Ensaio_Carga',
-            'ta': 25, 'polos': 4}
-    k_estator = {'Cobre': 234.5, 'Alumínio': 225.0}
-    dici.update(kwargs)
-    k_estator = k_estator[dici['material_estator']]
-    k_rotor = 225.0
-    dfc = deepcopy(df[dici['aba_carga']])
-
-    # Cálculo das resistências
-    termico = ensaio_termico(df, aba_termico='Ensaio_Termico_Carga')
-    dfc['D - T1@Temp OP'] = termico['Ensaio_Termico_Carga'].loc['Inicial',
-                                                                'D - R1@25C']*(dfc['Temperatura'] + k_estator)/(dici['ta']+k_estator)
-
-    # Cálculo do escorregamento
-    Ns = dfc['Frequencia']*120/dici['polos']
-    dfc['D - Escorregamento'] = (Ns - dfc['RPM'])/Ns
-    dfc['D - Escorregamento'] = dfc['D - Escorregamento'] * \
-        (dfc['Temperatura']+k_rotor)/(dici['ta'] + k_rotor)
-
-    # Cálculo da Potência Mecânica
-    dfc['D - Potencia Mecanica'] = (2*pi)/60*dfc['Torque']*dfc['RPM']
-
-    # Cálculo Pj1
-    dfc['D - Pj1'] = 3*dfc['D - T1@Temp OP']*dfc['Corrente']*dfc['Corrente']
-
-    # Cálculo Cos(phi)
-    dfc['D - Cos(phi)'] = dfc['Potencia'] / \
-        (dfc['Corrente']*dfc['Tensao']*np.sqrt(3))
-
-    # Cálculo de E
-    dfc['D - E'] = np.sqrt((dfc['Tensao'] - (np.sqrt(3)/2)*dfc['Corrente']*dfc['D - T1@Temp OP']*dfc['D - Cos(phi)'])
-                           ** 2 + ((np.sqrt(3)/2*dfc['Corrente']*dfc['D - T1@Temp OP'])*np.sqrt(1 - dfc['D - Cos(phi)']**2))**2)
-
-    df[dici['aba_carga']] = dfc
-    return df
-
-
-def IEEE112_Metodo_A(df, **kwargs):
-    pass
-
-
+'''
 def IEEE112_Metodo_B(df, **kwargs):
-    df = ensaio_vazio(df, **kwargs)
-    df = ensaio_carga(df, **kwargs)
-
-    dici = {'material_estator': 'Cobre',
-            'aba_carga': 'Ensaio_Carga',
-            'aba_vazio': 'Ensaio_Vazio',
-            'ta': 25, 'polos': 4,
-            'fora': [420],
-            'pu': 380
-            }
-    k_estator = {'Cobre': 234.5, 'Alumínio': 225.0}
-    dici.update(kwargs)
-    k_estator = k_estator[dici['material_estator']]
-    k_rotor = 225.0
-
-    dfc = deepcopy(df[dici['aba_carga']])
-    dfv = deepcopy(df[dici['aba_vazio']])
-
     # Perdas no Ferro - Correção E
     dfc['D - Perdas Ferro'] = dfv.loc[dici['pu'],
                                       'E - Perdas Ferro Tensao Nominal']*(dfc['D - E']/dici['pu'])**2
@@ -360,5 +401,10 @@ def IEEE112_Metodo_B(df, **kwargs):
 
     df[dici['aba_carga']] = dfc
     return df
+'''
 
-
+if __name__ == '__main__':
+    a = IEEE112MetodoA()
+    print(a.incertezas)
+    
+# %%
